76a57369518a7fead13f39267f1479ea3ae9cccd
Yunkang Tao <1461932145@qq.com>
2024-12-04 11:30:39 +0800

    update predict scripts

--- predict_t2v.py
+++ predict_t2v.py
@@ -28,7 +28,7 @@ GPU_memory_mode = "model_cpu_offload"
 
 # Config and model path
 config_path = "config/easyanimate_video_v5_magvit_multi_text_encoder.yaml"
-model_name = "models/Diffusion_Transformer/EasyAnimateV5-12b-zh-InP"
+model_name = "models/Diffusion_Transformer/EasyAnimateV5-7b-zh-CameraControl"
 
 # Choose the sampler in "Euler" "Euler A" "DPM++" "PNDM" and "DDIM"
 # EasyAnimateV1, V2 and V3 cannot use DDIM.
