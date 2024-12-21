b2bac838c6970e8cde4f04f943306258e97cc526
YunkangTao <1461932145@qq.com>
2024-12-21 21:41:29 +0800

    format

--- easyanimate/data/bucket_sampler.py
+++ easyanimate/data/bucket_sampler.py
@@ -1,7 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os
-from typing import (Generic, Iterable, Iterator, List, Optional, Sequence,
-                    Sized, TypeVar, Union)
+from typing import Generic, Iterable, Iterator, List, Optional, Sequence, Sized, TypeVar, Union
 
 import cv2
 import numpy as np
@@ -10,41 +9,78 @@ from PIL import Image
 from torch.utils.data import BatchSampler, Dataset, Sampler
 
 ASPECT_RATIO_512 = {
-    '0.25': [256.0, 1024.0], '0.26': [256.0, 992.0], '0.27': [256.0, 960.0], '0.28': [256.0, 928.0],
-    '0.32': [288.0, 896.0], '0.33': [288.0, 864.0], '0.35': [288.0, 832.0], '0.4': [320.0, 800.0],
-    '0.42': [320.0, 768.0], '0.48': [352.0, 736.0], '0.5': [352.0, 704.0], '0.52': [352.0, 672.0],
-    '0.57': [384.0, 672.0], '0.6': [384.0, 640.0], '0.68': [416.0, 608.0], '0.72': [416.0, 576.0],
-    '0.78': [448.0, 576.0], '0.82': [448.0, 544.0], '0.88': [480.0, 544.0], '0.94': [480.0, 512.0],
-    '1.0': [512.0, 512.0], '1.07': [512.0, 480.0], '1.13': [544.0, 480.0], '1.21': [544.0, 448.0],
-    '1.29': [576.0, 448.0], '1.38': [576.0, 416.0], '1.46': [608.0, 416.0], '1.67': [640.0, 384.0],
-    '1.75': [672.0, 384.0], '2.0': [704.0, 352.0], '2.09': [736.0, 352.0], '2.4': [768.0, 320.0],
-    '2.5': [800.0, 320.0], '2.89': [832.0, 288.0], '3.0': [864.0, 288.0], '3.11': [896.0, 288.0],
-    '3.62': [928.0, 256.0], '3.75': [960.0, 256.0], '3.88': [992.0, 256.0], '4.0': [1024.0, 256.0]
+    '0.25': [256.0, 1024.0],
+    '0.26': [256.0, 992.0],
+    '0.27': [256.0, 960.0],
+    '0.28': [256.0, 928.0],
+    '0.32': [288.0, 896.0],
+    '0.33': [288.0, 864.0],
+    '0.35': [288.0, 832.0],
+    '0.4': [320.0, 800.0],
+    '0.42': [320.0, 768.0],
+    '0.48': [352.0, 736.0],
+    '0.5': [352.0, 704.0],
+    '0.52': [352.0, 672.0],
+    '0.57': [384.0, 672.0],
+    '0.6': [384.0, 640.0],
+    '0.68': [416.0, 608.0],
+    '0.72': [416.0, 576.0],
+    '0.78': [448.0, 576.0],
+    '0.82': [448.0, 544.0],
+    '0.88': [480.0, 544.0],
+    '0.94': [480.0, 512.0],
+    '1.0': [512.0, 512.0],
+    '1.07': [512.0, 480.0],
+    '1.13': [544.0, 480.0],
+    '1.21': [544.0, 448.0],
+    '1.29': [576.0, 448.0],
+    '1.38': [576.0, 416.0],
+    '1.46': [608.0, 416.0],
+    '1.67': [640.0, 384.0],
+    '1.75': [672.0, 384.0],
+    '2.0': [704.0, 352.0],
+    '2.09': [736.0, 352.0],
+    '2.4': [768.0, 320.0],
+    '2.5': [800.0, 320.0],
+    '2.89': [832.0, 288.0],
+    '3.0': [864.0, 288.0],
+    '3.11': [896.0, 288.0],
+    '3.62': [928.0, 256.0],
+    '3.75': [960.0, 256.0],
+    '3.88': [992.0, 256.0],
+    '4.0': [1024.0, 256.0],
 }
 ASPECT_RATIO_RANDOM_CROP_512 = {
-    '0.42': [320.0, 768.0], '0.5': [352.0, 704.0], 
-    '0.57': [384.0, 672.0], '0.68': [416.0, 608.0], '0.78': [448.0, 576.0], '0.88': [480.0, 544.0], 
-    '0.94': [480.0, 512.0], '1.0': [512.0, 512.0], '1.07': [512.0, 480.0], 
-    '1.13': [544.0, 480.0], '1.29': [576.0, 448.0], '1.46': [608.0, 416.0], '1.75': [672.0, 384.0], 
-    '2.0': [704.0, 352.0],  '2.4': [768.0, 320.0]
+    '0.42': [320.0, 768.0],
+    '0.5': [352.0, 704.0],
+    '0.57': [384.0, 672.0],
+    '0.68': [416.0, 608.0],
+    '0.78': [448.0, 576.0],
+    '0.88': [480.0, 544.0],
+    '0.94': [480.0, 512.0],
+    '1.0': [512.0, 512.0],
+    '1.07': [512.0, 480.0],
+    '1.13': [544.0, 480.0],
+    '1.29': [576.0, 448.0],
+    '1.46': [608.0, 416.0],
+    '1.75': [672.0, 384.0],
+    '2.0': [704.0, 352.0],
+    '2.4': [768.0, 320.0],
 }
-ASPECT_RATIO_RANDOM_CROP_PROB = [
-    1, 2,
-    4, 4, 4, 4,
-    8, 8, 8,
-    4, 4, 4, 4,
-    2, 1
-]
+ASPECT_RATIO_RANDOM_CROP_PROB = [1, 2, 4, 4, 4, 4, 8, 8, 8, 4, 4, 4, 4, 2, 1]
 ASPECT_RATIO_RANDOM_CROP_PROB = np.array(ASPECT_RATIO_RANDOM_CROP_PROB) / sum(ASPECT_RATIO_RANDOM_CROP_PROB)
+
 
 def get_closest_ratio(height: float, width: float, ratios: dict = ASPECT_RATIO_512):
     aspect_ratio = height / width
     closest_ratio = min(ratios.keys(), key=lambda ratio: abs(float(ratio) - aspect_ratio))
     return ratios[closest_ratio], float(closest_ratio)
 
+
 def get_image_size_without_loading(path):
     with Image.open(path) as img:
         return img.size  # (width, height)
+
 
 class RandomSampler(Sampler[int]):
     r"""Samples elements randomly. If without replacement, then sample from a shuffled dataset.
@@ -61,8 +97,7 @@ class RandomSampler(Sampler[int]):
     data_source: Sized
     replacement: bool
 
-    def __init__(self, data_source: Sized, replacement: bool = False,
-                 num_samples: Optional[int] = None, generator=None) -> None:
+    def __init__(self, data_source: Sized, replacement: bool = False, num_samples: Optional[int] = None, generator=None) -> None:
         self.data_source = data_source
         self.replacement = replacement
         self._num_samples = num_samples
@@ -100,15 +135,16 @@ class RandomSampler(Sampler[int]):
                 xx = torch.randperm(n, generator=generator).tolist()
                 if self._pos_start >= n:
                     self._pos_start = 0
-                print("xx top 10", xx[:10], self._pos_start)
+                # print("xx top 10", xx[:10], self._pos_start)
                 for idx in range(self._pos_start, n):
                     yield xx[idx]
                     self._pos_start = (self._pos_start + 1) % n
                 self._pos_start = 0
-            yield from torch.randperm(n, generator=generator).tolist()[:self.num_samples % n]
+            yield from torch.randperm(n, generator=generator).tolist()[: self.num_samples % n]
 
     def __len__(self) -> int:
         return self.num_samples
+
 
 class AspectRatioBatchImageSampler(BatchSampler):
     """A sampler wrapper for grouping images with similar aspect ratio into a same batch.
@@ -121,23 +157,14 @@ class AspectRatioBatchImageSampler(BatchSampler):
             its size would be less than ``batch_size``.
         aspect_ratios (dict): The predefined aspect ratios.
     """
+
     def __init__(
-        self,
-        sampler: Sampler,
-        dataset: Dataset,
-        batch_size: int,
-        train_folder: str = None,
-        aspect_ratios: dict = ASPECT_RATIO_512,
-        drop_last: bool = False,
-        config=None,
-        **kwargs
+        self, sampler: Sampler, dataset: Dataset, batch_size: int, train_folder: str = None, aspect_ratios: dict = ASPECT_RATIO_512, drop_last: bool = False, config=None, **kwargs
     ) -> None:
         if not isinstance(sampler, Sampler):
-            raise TypeError('sampler should be an instance of ``Sampler``, '
-                            f'but got {sampler}')
+            raise TypeError('sampler should be an instance of ``Sampler``, ' f'but got {sampler}')
         if not isinstance(batch_size, int) or batch_size <= 0:
-            raise ValueError('batch_size should be a positive integer value, '
-                             f'but got batch_size={batch_size}')
+            raise ValueError('batch_size should be a positive integer value, ' f'but got batch_size={batch_size}')
         self.sampler = sampler
         self.dataset = dataset
         self.train_folder = train_folder
@@ -145,9 +172,9 @@ class AspectRatioBatchImageSampler(BatchSampler):
         self.aspect_ratios = aspect_ratios
         self.drop_last = drop_last
         self.config = config
-        # buckets for each aspect ratio 
+        # buckets for each aspect ratio
         self._aspect_ratio_buckets = {ratio: [] for ratio in aspect_ratios}
-        # [str(k) for k, v in aspect_ratios] 
+        # [str(k) for k, v in aspect_ratios]
         self.current_available_bucket_keys = list(aspect_ratios.keys())
 
     def __iter__(self):
@@ -165,11 +192,11 @@ class AspectRatioBatchImageSampler(BatchSampler):
 
                     width, height = get_image_size_without_loading(image_dir)
 
-                    ratio = height / width # self.dataset[idx]
+                    ratio = height / width  # self.dataset[idx]
                 else:
                     height = int(height)
                     width = int(width)
-                    ratio = height / width # self.dataset[idx]
+                    ratio = height / width  # self.dataset[idx]
             except Exception as e:
                 print(e)
                 continue
@@ -184,6 +211,7 @@ class AspectRatioBatchImageSampler(BatchSampler):
                 yield bucket[:]
                 del bucket[:]
 
+
 class AspectRatioBatchSampler(BatchSampler):
     """A sampler wrapper for grouping images with similar aspect ratio into a same batch.
 
@@ -195,6 +223,7 @@ class AspectRatioBatchSampler(BatchSampler):
             its size would be less than ``batch_size``.
         aspect_ratios (dict): The predefined aspect ratios.
     """
+
     def __init__(
         self,
         sampler: Sampler,
@@ -205,14 +234,12 @@ class AspectRatioBatchSampler(BatchSampler):
         aspect_ratios: dict = ASPECT_RATIO_512,
         drop_last: bool = False,
         config=None,
-        **kwargs
+        **kwargs,
     ) -> None:
         if not isinstance(sampler, Sampler):
-            raise TypeError('sampler should be an instance of ``Sampler``, '
-                            f'but got {sampler}')
+            raise TypeError('sampler should be an instance of ``Sampler``, ' f'but got {sampler}')
         if not isinstance(batch_size, int) or batch_size <= 0:
-            raise ValueError('batch_size should be a positive integer value, '
-                             f'but got batch_size={batch_size}')
+            raise ValueError('batch_size should be a positive integer value, ' f'but got batch_size={batch_size}')
         self.sampler = sampler
         self.dataset = dataset
         self.video_folder = video_folder
@@ -221,9 +248,9 @@ class AspectRatioBatchSampler(BatchSampler):
         self.aspect_ratios = aspect_ratios
         self.drop_last = drop_last
         self.config = config
-        # buckets for each aspect ratio 
+        # buckets for each aspect ratio
         self._aspect_ratio_buckets = {ratio: [] for ratio in aspect_ratios}
-        # [str(k) for k, v in aspect_ratios] 
+        # [str(k) for k, v in aspect_ratios]
         self.current_available_bucket_keys = list(aspect_ratios.keys())
 
     def __iter__(self):
@@ -245,14 +272,14 @@ class AspectRatioBatchSampler(BatchSampler):
                     cap = cv2.VideoCapture(video_dir)
 
                     # 获取视频尺寸
-                    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))   # 浮点数转换为整数
+                    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 浮点数转换为整数
                     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 浮点数转换为整数
-                    
-                    ratio = height / width # self.dataset[idx]
+
+                    ratio = height / width  # self.dataset[idx]
                 else:
                     height = int(height)
                     width = int(width)
-                    ratio = height / width # self.dataset[idx]
+                    ratio = height / width  # self.dataset[idx]
             except Exception as e:
                 print(e)
                 continue
@@ -267,6 +294,7 @@ class AspectRatioBatchSampler(BatchSampler):
                 yield bucket[:]
                 del bucket[:]
 
+
 class AspectRatioBatchImageVideoSampler(BatchSampler):
     """A sampler wrapper for grouping images with similar aspect ratio into a same batch.
 
@@ -279,20 +307,11 @@ class AspectRatioBatchImageVideoSampler(BatchSampler):
         aspect_ratios (dict): The predefined aspect ratios.
     """
 
-    def __init__(self,
-                 sampler: Sampler,
-                 dataset: Dataset,
-                 batch_size: int,
-                 train_folder: str = None,
-                 aspect_ratios: dict = ASPECT_RATIO_512,
-                 drop_last: bool = False
-                ) -> None:
+    def __init__(self, sampler: Sampler, dataset: Dataset, batch_size: int, train_folder: str = None, aspect_ratios: dict = ASPECT_RATIO_512, drop_last: bool = False) -> None:
         if not isinstance(sampler, Sampler):
-            raise TypeError('sampler should be an instance of ``Sampler``, '
-                            f'but got {sampler}')
+            raise TypeError('sampler should be an instance of ``Sampler``, ' f'but got {sampler}')
         if not isinstance(batch_size, int) or batch_size <= 0:
-            raise ValueError('batch_size should be a positive integer value, '
-                             f'but got batch_size={batch_size}')
+            raise ValueError('batch_size should be a positive integer value, ' f'but got batch_size={batch_size}')
         self.sampler = sampler
         self.dataset = dataset
         self.train_folder = train_folder
@@ -302,10 +321,7 @@ class AspectRatioBatchImageVideoSampler(BatchSampler):
 
         # buckets for each aspect ratio
         self.current_available_bucket_keys = list(aspect_ratios.keys())
-        self.bucket = {
-            'image':{ratio: [] for ratio in aspect_ratios}, 
-            'video':{ratio: [] for ratio in aspect_ratios}
-        }
+        self.bucket = {'image': {ratio: [] for ratio in aspect_ratios}, 'video': {ratio: [] for ratio in aspect_ratios}}
 
     def __iter__(self):
         for idx in self.sampler:
@@ -324,11 +340,11 @@ class AspectRatioBatchImageVideoSampler(BatchSampler):
 
                         width, height = get_image_size_without_loading(image_dir)
 
-                        ratio = height / width # self.dataset[idx]
+                        ratio = height / width  # self.dataset[idx]
                     else:
                         height = int(height)
                         width = int(width)
-                        ratio = height / width # self.dataset[idx]
+                        ratio = height / width  # self.dataset[idx]
                 except Exception as e:
                     print(e)
                     continue
@@ -356,14 +372,14 @@ class AspectRatioBatchImageVideoSampler(BatchSampler):
                         cap = cv2.VideoCapture(video_dir)
 
                         # 获取视频尺寸
-                        width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))   # 浮点数转换为整数
+                        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 浮点数转换为整数
                         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 浮点数转换为整数
-                        
-                        ratio = height / width # self.dataset[idx]
+
+                        ratio = height / width  # self.dataset[idx]
                     else:
                         height = int(height)
                         width = int(width)
-                        ratio = height / width # self.dataset[idx]
+                        ratio = height / width  # self.dataset[idx]
                 except Exception as e:
                     print(e)
                     continue
--- scripts/train_inpainting_with_mask.sh
+++ scripts/train_inpainting_with_mask.sh
@@ -1,9 +1,9 @@
 export MODEL_NAME="models/Diffusion_Transformer/EasyAnimateV5-7b-zh-InP"
-export DATASET_NAME="datasets/z_mini_datasets_warped_videos_2_3"
-export DATASET_META_NAME="datasets/z_mini_datasets_warped_videos_2_3/metadata.json"
+export DATASET_NAME="/mnt/chenyang_lei/Datasets/easyanimate_dataset/z_mini_datasets_warped_videos_2_3_test"
+export DATASET_META_NAME="/mnt/chenyang_lei/Datasets/easyanimate_dataset/z_mini_datasets_warped_videos_2_3_test/metadata.json"
 export NCCL_IB_DISABLE=1
 export NCCL_P2P_DISABLE=1
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 NCCL_DEBUG=INFO
 
@@ -18,19 +18,19 @@ accelerate launch \
   --train_data_dir=$DATASET_NAME \
   --train_data_meta=$DATASET_META_NAME \
   --config_path "config/easyanimate_video_v5_magvit_multi_text_encoder.yaml" \
-  --video_sample_size=256 \
+  --video_sample_size=512 \
   --video_sample_stride=3 \
-  --video_sample_n_frames=25 \
-  --train_batch_size=1 \
+  --video_sample_n_frames=49 \
+  --train_batch_size=2 \
   --gradient_accumulation_steps=1 \
-  --dataloader_num_workers=0 \
+  --dataloader_num_workers=8 \
   --num_train_epochs=100 \
-  --checkpointing_steps=100 \
+  --checkpointing_steps=1000 \
   --learning_rate=2e-05 \
   --lr_scheduler="constant_with_warmup" \
   --lr_warmup_steps=100 \
   --seed=42 \
-  --output_dir="output_dir_20241220_inpainting_with_mask_test" \
+  --output_dir="output_dir_20241221_inpainting_with_mask" \
   --gradient_checkpointing \
   --mixed_precision="bf16" \
   --adam_weight_decay=5e-3 \
